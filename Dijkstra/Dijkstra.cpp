a924e32eef3f389affe8a51584d9cca5c2afc9f4
최가온(Gaon Choi) <choigaon1028@gmail.com>
2025-04-25 09:58:09 +0900

    docs : reimplement dijkstra

--- Dijkstra/Dijkstra.cpp
+++ Dijkstra/Dijkstra.cpp
@@ -1,44 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
-#include <tuple>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> Dijkstra(const vector<vector<pair<int, int>>>& graph, int src) {
-    int n = graph.size(); // 정점 개수
-    vector<int> dist(n, INT_MAX); // 거리 배열 (INT_MAX로 초기화)
+const int INF = 1e9;
 
-    // 최소 힙 (거리, 정점)
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+vector<int> dijkstra(const vector<vector<pair<int, int>>> &graph, int start) {
+	int graph_size = graph.size();
+	
+	// 거리 배열
+	vector<int> dist(graph_size, INF);
+	// 우선순위 큐
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-    // 탐색을 시작할 정점에 대해 거리를 0으로 설정하고 힙에 삽입
-    dist[src] = 0;
-    pq.push({0, src});
+	dist[start] = 0;
+	pq.push({0, start});
 
-    // 힙 내부가 비어있기 전까지 계속 반복
-    while (!pq.empty()) {
-        int curr_dist = pq.top().first;
-        int u = pq.top().second;
-        pq.pop();
+	while(!pq.empty()) {
+		int curr_dist = pq.top().first;
+		int u = pq.top().second;
+		pq.pop();
 
-        // 이미 더 짧은 거리로 방문했다면 스킵
-        if (curr_dist > dist[u]) continue;
+		for (const pair<int, int> adj : graph[u]) {
+			int v = adj.first;
+			int v_cost = adj.second;
 
-        for (const pair<int, int>& p : graph[u]) {
-            int v = p.first, cost = p.second;
+			int new_cost = curr_dist + v_cost;
+			if (new_cost < dist[v]) {
+				dist[v] = new_cost;
+				pq.push({new_cost, v});
+			}
+		}
+	}
 
-            // 새로 갱신된 거리
-            int new_dist = curr_dist + cost;
-
-            // 새로 계산된 거리가 기존의 dist 배열에 저장된 거리 값보다 작은 경우 갱신
-            if (new_dist < dist[v]) {
-                dist[v] = new_dist;
-                pq.push({new_dist, v});
-            }
-        }
-    }
-
-    return dist;
+	return dist;
 }
